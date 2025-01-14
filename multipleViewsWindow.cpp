32ef58c5ec24f7d0405eeae733dbefff9b3446d5
Thomas <tomn1345@gmail.com>
2025-01-14 19:21:39 +0100

    Some clearing

--- multipleViewsWindow.cpp
+++ multipleViewsWindow.cpp
@@ -24,22 +24,18 @@ void MultipleViewsWindow::createSideMenu()
     sideMenu = new QWidget;
     menuLayout = new QVBoxLayout(sideMenu);
 
-    // Création des boutons du menu
     QPushButton* page1Button = new QPushButton("Executed scan");
     QPushButton* page2Button = new QPushButton("Order book scan");
     QPushButton* page3Button = new QPushButton("Raw Data");
 
-    // Style pour le menu latéral
     sideMenu->setMaximumWidth(200);
     sideMenu->setStyleSheet("background-color: #f0f0f0;");
 
-    // Ajout des boutons au layout du menu
     menuLayout->addWidget(page1Button);
     menuLayout->addWidget(page2Button);
     menuLayout->addWidget(page3Button);
-    menuLayout->addStretch(); // Ajoute un espace extensible en bas
+    menuLayout->addStretch(); 
 
-    // Connexion des boutons
     connect(page1Button, &QPushButton::clicked, [this]() { changePage(0); });
     connect(page2Button, &QPushButton::clicked, [this]() { changePage(1); });
     connect(page3Button, &QPushButton::clicked, [this]() { changePage(2); });
@@ -47,20 +43,19 @@ void MultipleViewsWindow::createSideMenu()
 
 void MultipleViewsWindow::createPages()
 {
-    // Création des pages exemple
     QWidget* page1 = new QWidget;
     QWidget* page2 = new QWidget;
     QWidget* page3 = new QWidget;
 
     // Exemple de contenu pour les pages
     QVBoxLayout* layout1 = new QVBoxLayout(page1);
-    layout1->addWidget(new QLabel("Contenu de la page 1"));
+    layout1->addWidget(new QLabel("Chart on executed data"));
 
     QVBoxLayout* layout2 = new QVBoxLayout(page2);
-    layout2->addWidget(new QLabel("Contenu de la page 2"));
+    layout2->addWidget(new QLabel("Chart on pretrade data"));
 
     QVBoxLayout* layout3 = new QVBoxLayout(page3);
-    layout3->addWidget(new QLabel("Contenu de la page 3"));
+    layout3->addWidget(new QLabel("Raw data search"));
 
     // Ajout des pages au widget empilé
     stackedWidget->addWidget(page1);
